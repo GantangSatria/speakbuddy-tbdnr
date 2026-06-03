8bcd09b373afc3edf48cac19c7b7a63e7a5c5f50
GantangSatria <gantang610@gmail.com>
2026-06-03 20:30:52 +0700

    feat(consultation): add CreateConsultationDto and UpdateConsultationDto for consultation data transfer

---
+++ src/presentation/dtos/consultation.dto.ts
@@ -0,0 +1,19 @@
+import { IsBoolean, IsDateString, IsEnum, IsMongoId, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ConsultationStatus } from 'src/domain/entities/consultation.entity';
+
+export class CreateConsultationDto {
+  @IsMongoId() therapist_user_id!: string;
+  @IsOptional() @IsString() child_name?: string;
+  @IsOptional() @IsNumber() child_age?: number;
+  @IsOptional() @IsString() child_sex?: string;
+  @IsDateString() date!: string;
+  @IsString() time_slot!: string;
+  @IsOptional() @IsNumber() fee?: number;
+  @IsOptional() @IsString() payment_method?: string;
+}
+
+export class UpdateConsultationDto {
+  @IsOptional() @IsBoolean() is_paid?: boolean;
+  @IsOptional() @IsString() payment_method?: string;
+  @IsOptional() @IsEnum(ConsultationStatus) status?: ConsultationStatus;
+}
